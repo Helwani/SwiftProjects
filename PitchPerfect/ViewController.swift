9bec08217b11e92d121018b4b9f6eb2ab48a164a
Helwani <omarhelwani@Omars-MacBook-Air.local>
2018-12-30 14:28:54 +0100

    Added stop recording print

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -26,6 +26,9 @@ class ViewController: UIViewController {
         print("Record button has been pressed")
         recordingLabel.text = "Recording in progress"
     }
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop recording button has been pressed")
+    }
     
 }
 
