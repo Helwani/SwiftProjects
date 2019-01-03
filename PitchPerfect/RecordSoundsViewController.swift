85db8d9399618efe031704e760fc8861dfe2cf77
Helwani <omarhelwani@Omars-MacBook-Air.local>
2019-01-03 07:14:13 +0100

    Solving issues of last commit

---
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  RecordSoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Omar Helwani on 29/12/2018.
+//  Copyright © 2018 Omar Helwani. All rights reserved.
+//
+
+import UIKit
+
+class RecordSoundsViewController: UIViewController {
+    
+    @IBOutlet weak var recordingButton: UIButton!
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button has been pressed")
+        recordingLabel.text = "Recording in progress"
+        stopRecordingButton.isEnabled = true
+        recordingButton.isEnabled = false
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop recording button has been pressed")
+        recordingLabel.text = "Tap to record"
+        stopRecordingButton.isEnabled = false
+        recordingButton.isEnabled = true
+    }
+    
+}
+
+
